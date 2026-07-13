50272b80c3e163650e75936f2e5a6048e27c9147
hhao99 <hhao99@gmail.com>
2026-07-13 10:07:20 -0500

    springboot project starter

--- src/main/kotlin/rp/blog/BlogApplication.kt
+++ src/main/kotlin/rp/blog/BlogApplication.kt
@@ -2,10 +2,20 @@ package rp.blog
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.CommandLineRunner
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class BlogApplication
+class BlogApplication {
+	@Bean
+	fun init() = CommandLineRunner {
+		println("===> Application initializing...")
+		
+		println("===> Application initialized ")
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<BlogApplication>(*args)
 }
+
