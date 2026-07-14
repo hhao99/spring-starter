5a7e340172ad0c8a9a0dca0409bf1f449d796d6d
hhao99 <hhao99@gmail.com>
2026-07-14 00:26:21 -0500

    crossorigin

--- src/main/kotlin/rp/blog/controllers/UserController.kt
+++ src/main/kotlin/rp/blog/controllers/UserController.kt
@@ -6,8 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.CrossOrigin
+
 
 @RestController
+@CrossOrigin(origins=["http://localhost:4200"])
 @RequestMapping("/api/v1/users")
 class UserController(private val service: UserService){
     @GetMapping
