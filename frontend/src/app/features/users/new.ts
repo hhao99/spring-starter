e5d0a97c057986c912b92a1d4df593ff480f3b48
hhao99 <hhao99@gmail.com>
2026-07-15 10:16:01 -0500

    material basic integrated

--- frontend/src/app/features/users/new.ts
+++ frontend/src/app/features/users/new.ts
@@ -1,15 +1,31 @@
 import { Component, signal, inject } from '@angular/core';
 import { form,FormField, FormRoot } from '@angular/forms/signals'
 import { NewUser, UserService } from './user.service'
+
+// material
+import { MatButtonModule } from '@angular/material/button';
+import {  MatInputModule } from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'user-new',
-  imports: [FormRoot, FormField],
+  imports: [FormRoot, FormField,MatButtonModule,MatIconModule, MatInputModule, MatFormFieldModule],
   template: `
     <div>
       <form [formRoot]="userForm">
-        <label> Name:
-        <input type='text' [formField]="userForm.name"/>
-</label>
+        <mat-form-field>
+          <mat-label> Name:</mat-label>
+          <input
+            class="w-50 border-b-1" 
+            matInput type='text' [formField]="userForm.name"/>
+            @if(this.newUserModel().name !== '') {
+              <button matSuffix matIconButton (click)="this.newUserModel.set({name: ''})">
+                <mat-icon>close</mat-icon>
+              </button>
+            }
+            <button matButton type='submit'>save</button>
+          </mat-form-field>
+
       </form>
       
     </div>
