2f34a49b6593743326d25ae7ae4fbee20dbfd372
hhao99 <hhao99@gmail.com>
2026-07-16 09:39:57 -0500

    integrating nextjs

--- front-next/app/users/page.tsx
+++ front-next/app/users/page.tsx
@@ -1,10 +1,13 @@
 
 export default async function Home() {
-  const posts = await (await fetch('http://localhost:8080/api/v1/users')).json();
+  const users = await (await fetch('http://localhost:8080/api/v1/users')).json();
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
     <h1>User page</h1> 
-    <div>Post length: {posts?.length}</div>
+    <div>User length: {users?.length}</div>
+    <ul>
+      {users.length >  0 ? users.map( user => <li key={user.id}>{user.name}</li> ) : "no user right now"}
+    </ul>
     </div>
   );
 }
